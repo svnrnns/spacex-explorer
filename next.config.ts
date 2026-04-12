9f4678b7c80f7860a796eefef74652c60852ba3a
seven rings <haruueel@gmail.com>
2026-04-12 11:52:00 +0200

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  devIndicators: false,
+  /**
+   * Forces blocking metadata for all requests. Uses RegExp * to
+   * match all user agents and ensure type safety.
+   * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#streaming-metadata
+   */
+  htmlLimitedBots: /.*/, // Use a RegExp literal
 };
 
 export default nextConfig;
